de40b69667e96877f2f3eb2727ec471f578d8e4d
Anuj Kumar Sony <anujkumarrr0@gmail.com>
2024-07-19 23:42:28 +0530

    update

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -13,8 +13,15 @@ function App() {
     //console.log("value added", Math.random());
     
     if (counter < 20) {
-      counter = counter + 1
-      setCounter(counter)
+      //counter = counter + 1
+      setCounter(counter+1)
+      /*when we want to increase value one by one then we use this // it's a interview question when we want to add duplicate increament in our code 
+      setCounter(prevCounter => prevCounter + 1)  //we can write anything on prevCounter
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      */
+
       console.log("clicked", counter );
     }
   }
